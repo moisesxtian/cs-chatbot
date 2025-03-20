dd4a5a90060bb629279547d4ea5d38e85cab0dae
Hyx <christiansmoises023@gmail.com>
2025-03-20 15:58:51 +0800

    ,

--- react/vite.config.js
+++ react/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",  // Ensures Vite loads assets correctly
   plugins: [react(),tailwindcss(),],
+  build: {
+    outDir: "dist", 
+    emptyOutDir: true
+  }
 })
